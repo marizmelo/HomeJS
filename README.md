0482575730b8aa8c3a4229654c09f3d8ab4ac0a2
Mariz Melo <mm@xchema.com>
2013-04-17 16:07:19 -0700

    gestures instructions, menu options

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ HomeJS
 ======
 
 Facebook Home Prototype in JavaScript
+
+
+Third-party
+===========
+Swipeshow by https://github.com/rstacruz/swipeshow
+Hand gesture icons by **GestureWorks** - http://gestureworks.com/
--- xtyle/js/homejs.js
+++ xtyle/js/homejs.js
@@ -1,5 +1,26 @@
 $(function(){
-    $( "#user" ).draggable({ revert: true });
+    
+    /* PROFILE BUTTON */
+    var selected = 0;
+    $( "#user" ).delay(500).fadeIn("slow").draggable({ revert: true });
+    $( "#user" ).on("click", function(){
+      if (!selected) {
+        selected = 1;
+          $(".menu").fadeIn("fast");
+          $("#messages").animate({"marginLeft" : "-65px"}, 100);
+            $("#last").animate({ "marginLeft" : "80px"}, 100);
+              $("#apps").animate({ "marginTop" : "-70px"}, 100);
+        
+      } else {
+        selected = 0;
+          $(".menu").fadeOut("fast");
+          $("#last").animate({ "marginLeft" : "0"}, 100);
+            $("#apps").animate({ "marginTop" : "0"}, 100);
+              $("#messages").animate({"marginLeft" : "0"}, 100);
+      }
+    });
+    
+    /* CLOCK */
     var startTime = function ()
     {
       var today=new Date();
@@ -20,7 +41,7 @@ $(function(){
   
     startTime();
 
-
+    /* SLIDESHOW */
     $(".version").text($.swipeshow.version);
     $(".slideshow").swipeshow({ autostart: false, mouse: true, interval: 6000 });
 });
